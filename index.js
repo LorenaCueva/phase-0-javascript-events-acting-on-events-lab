eb71ab36fbcc737d747cc7019246dd6822109e09
Lorena Cueva <lorenacueva@gmail.com>
2022-03-21 16:11:59 -0700

    commit tree 50ee6d58fecb029c902a14ddc89c6c4cf925a085

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", e => {
+    switch(e.key) {
+        case "ArrowLeft" : moveDodgerLeft();
+        break;
+        case "ArrowRight" : moveDodgerRight();
+        break;
+  }});
+
+  function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if(left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+  }
